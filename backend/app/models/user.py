84bdad3d8845c6bbc295f0bd559cfffbde6c45d9
IshhanKheria <ishhankheria06@gmail.com>
2026-03-30 12:34:10 +0530

    feat: implemented google auth

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -8,3 +8,5 @@ class User(Base):
     username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
+    google_id = Column(String, nullable=True)
+    auth_provider = Column(String, default="email")
